b685dd8aecad3e5594f51944e30bcb7d4316b317
Jolkert <jolkertt@gmail.com>
2024-05-21 00:42:11 -0400

    leaderboard alias

--- src/command/rps.rs
+++ src/command/rps.rs
@@ -135,7 +135,7 @@ pub async fn challenge(
 
 /// View the Rock, Paper, Scissors leaderboard for this server
 #[allow(clippy::too_many_lines)] // this hurts -morgan 2024-05-20
-#[poise::command(slash_command, prefix_command, guild_only)]
+#[poise::command(aliases("lb"), slash_command, prefix_command, guild_only)]
 pub async fn leaderboard(
 	ctx: Context<'_>,
 	#[description = "Specify a user to see their specific score"] user: Option<Member>,
